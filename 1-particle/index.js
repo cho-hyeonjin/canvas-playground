00dbc8919596aa95a19a4739aa7137374b35e425
cho-hyeonjin <gusls2005@gmail.com>
2024-04-16 16:41:07 +0900

    ✏️Drawing Particle & define 🤹🏻‍♀️Particle Animate Function using 'window.requestAnimationFrame()' API

--- 1-particle/index.js
+++ 1-particle/index.js
@@ -1,7 +1,9 @@
 const canvas = document.querySelector("canvas");
 
 const ctx = canvas.getContext("2d");
-console.log(ctx.canvas);
+// 💡 canvas의 size를 다루는 방식은 2가지인데, 이 2가지를 함께 다뤄줘야 한다.
+//   1. css에서 canvas 선택자로 조절하는 방식
+//   2. canvas 객체에 내장된 canvas.width와 canvas.height 속성값을 조절하는 방식. (default: 300px, 150px)
 
 /** Device Pixels Ratio - 하나의 css pixel을 그릴 때 사용되는 기기의 픽셀 수 - 높을수록 선명 */
 const dpr = window.devicePixelRatio;
@@ -11,23 +13,59 @@ const canvasWidth = 300;
 const canvasHeight = 300;
 
 /** style(css)로 canvas 사이즈 변경하기 */
-// canvas.style.width =  + "px";
-// canvas.style.height = 300 + "px"; // canvas의 기본값음 150인데 css로 300으로 변경됨
 canvas.style.width = canvasWidth + "px";
 canvas.style.height = canvasHeight + "px";
 
 /** canvas의 자체 속성 canvas.width와 canvas.heitght로 canvas 사이즈 변경하기 */
-// canvas.width = 300;
-// canvas.height = 300; // 이 작업 안 해주면 아래 ctx.fillRect로 만든 사각형이 세로로 긴 직사각형이 됨. (style(css)로 height를 늘려줬으니까)
 canvas.width = canvasWidth * dpr;
 canvas.height = canvasHeight * dpr;
 ctx.scale(dpr, dpr);
 
-/** canvas의 사이즈가 css로 지정한 canvas의 사이즈보다 작을 때 - ctx.fillRect로 만든 사각형의 화질이 떨어지게 됨. (canvas의 크기가 100, 100인데 style에서 설정한 canvas는 300, 300이니까 300에 맞춰서 1픽셀의 크기가 넓어진 것!) */
-// canvas.width = 100;
-// canvas.height = 100;
+/** 원 그리기 - 각도는 degree가 아닌 radian */
 
-ctx.fillRect(10, 10, 50, 50);
-// 💡 canvas의 size를 다루는 방식은 2가지인데, 이 2가지 방식을 함께 다뤄줘야 한다.
-//   1. css값을 직접 추정해서 canvas size를 조절하는 방식
-//   2. canvas 자체 속성인 canvas.width와 canvas.height를 조절하는 방식. (default: 300px, 150px)
+/** 채워진 원 */
+// ctx.beginPath(); // 그리기 시작
+// ctx.arc(100, 100, 50, 0, (Math.PI / 180) * 360);
+// ctx.fillStyle = "blue";
+// ctx.fill(); // 채워진 원 / 반원 그릴 때
+// ctx.closePath(); // 그리기 끝
+
+// /** 안채워진 원 */
+// ctx.beginPath();
+// // ctx.arc(100, 100, 50, 0, (Math.PI / 180) * 360); // 원
+// ctx.arc(100, 100, 50, 0, (Math.PI / 180) * 180); // 반원(호)
+// ctx.stroke();
+// ctx.closePath(); // 그리기 끝
+
+/** 도형 움직이기 - 애니메이션 (using RequestAnimaition을 이용하여 매 프레임마다 우리가 만든 도형의 위치가 변하는 함수 호출하기)  */
+/** Particle 클래스 정의 - 여러 개의 Particle을 만들 수 있도록 */
+class Particle {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 180 * 360);
+    ctx.fillStyle = "#0b6623";
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
+const x = 100;
+const y = 100;
+const radius = 50;
+const particle = new Particle(x, y, radius);
+
+/** 애니메이션 함수 정의 */
+function animate() {
+  window.requestAnimationFrame(animate); // 매 프레임마다
+
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight); // 이전 도형이 지워지고
+
+  particle.draw(); // 위치가 변경되 도형이 새로 그려지는 방식
+}
+/** 애니메이션 함수 실행 */
+animate();
