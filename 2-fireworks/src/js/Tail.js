2c27d5e58779178b098870dd96fc524e7d79595a
cho-hyeonjin <gusls2005@gmail.com>
2024-04-18 09:01:09 +0900

    ♻️Refactored ☄️Tail update method for a more realistic movment✨

--- 2-fireworks/src/js/Tail.js
+++ 2-fireworks/src/js/Tail.js
@@ -1,4 +1,5 @@
 import CanvasOption from "./CanvasOption.js";
+import { randomNumBetween } from "./utils.js";
 
 export default class Tail extends CanvasOption {
   constructor(x, vy, color) {
@@ -7,6 +8,7 @@ export default class Tail extends CanvasOption {
     this.y = this.canvasHeight;
     this.vy = vy;
     this.color = color;
+    this.angle = randomNumBetween(0, 2);
 
     /** 중력 - 위로 올라갈수록(requestAnimation 실행 횟수 늘어날수록) 작아지게 */
     /** 마찰력 - 위로 올라갈수록 마찰력 커지게 해서 화면 밖으로 나가기 전에 멈추게 */
@@ -17,13 +19,17 @@ export default class Tail extends CanvasOption {
     this.vy *= this.friction;
     this.y += this.vy;
 
+    /** 꼬리 상승 시 움직임 자연스럽게 (일직선은 부자연스러움) */
+    this.angle += 1;
+    this.x += Math.cos(this.angle) * this.vy * 0.2;
+
     /** 꼬리가 상승함에 따라 옅어지다 없어지게 */
     this.opacity = -this.vy * 0.1;
   }
   draw() {
     this.ctx.fillStyle = `rgba(${this.color}, ${this.opacity})`; // Particle과 색상값을 공유해야 하므로, Particle에서 draw에 사용된 color값 형식과 통일시켜준다.
     this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, 1, 0, Math.PI * 2);
+    this.ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
     this.ctx.fill();
     this.ctx.closePath();
   }
