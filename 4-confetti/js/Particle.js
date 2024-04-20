8d259f1de46cf88150c2defab17ee499b062610e
cho-hyeonjin <gusls2005@gmail.com>
2024-04-20 14:18:24 +0900

    ♻️Refactor: Implementing Particle Class using 'randomNumBetween' util module

--- 4-confetti/js/Particle.js
+++ 4-confetti/js/Particle.js
@@ -1,13 +1,23 @@
+import { randomNumBetween } from "./utils.js";
+
 export default class Particle {
   constructor(x, y) {
+    this.angle = (Math.PI / 180) * randomNumBetween(0, 360);
+    this.radius = 3;
     this.x = x;
     this.y = y;
+
+    this.vx = this.radius * Math.cos(this.angle);
+    this.vy = this.radius * Math.sin(this.angle);
 
     this.width = 30;
     this.height = 30;
   }
 
-  update() {}
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
   draw(ctx) {
     ctx.fillStyle = "yellow";
     ctx.fillRect(this.x, this.y, this.width, this.height);
