a921b11236635fa27f5d52790783229345b79efa
cho-hyeonjin <gusls2005@gmail.com>
2024-04-21 18:54:40 +0900

    Implementation Wall Colliding detection

--- 5-minigame/js/Player.js
+++ 5-minigame/js/Player.js
@@ -1,4 +1,5 @@
 import App from "./App.js";
+import BoundingBox from "./BoundingBox.js";
 
 export default class Player {
   constructor() {
@@ -7,6 +8,13 @@ export default class Player {
     this.y = App.height * 0.5;
     this.width = 130;
     this.height = this.width * (96 / 140);
+
+    this.boundingBox = new BoundingBox(
+      this.x + 10,
+      this.y + 16,
+      this.width - 20,
+      this.height - 20
+    );
 
     this.counter = 0;
     this.frameX = 0;
@@ -28,6 +36,7 @@ export default class Player {
     }
     this.vy += this.gravity;
     this.y += this.vy;
+    this.boundingBox.y = this.y + 16;
   }
 
   draw() {
@@ -42,5 +51,7 @@ export default class Player {
       this.width,
       this.height
     );
+
+    this.boundingBox.draw();
   }
 }
--- 5-minigame/js/Wall.js
+++ 5-minigame/js/Wall.js
@@ -1,4 +1,5 @@
 import App from "./App.js";
+import BoundingBox from "./BoundingBox.js";
 import { randomNumBetween } from "./util.js";
 
 export default class Wall {
@@ -26,6 +27,19 @@ export default class Wall {
 
     this.generatedNext = false;
     this.gapNextX = App.width * randomNumBetween(0.6, 0.75);
+
+    this.boundingBox1 = new BoundingBox(
+      this.x + 30,
+      this.y1 + 30,
+      this.width - 60,
+      this.height - 60
+    );
+    this.boundingBox2 = new BoundingBox(
+      this.x + 30,
+      this.y2 + 30,
+      this.width - 60,
+      this.height - 60
+    );
   }
 
   get isOutside() {
@@ -36,8 +50,16 @@ export default class Wall {
     return !this.generatedNext && this.x + this.width < this.gapNextX;
   }
 
+  isColliding(target) {
+    return (
+      this.boundingBox1.isColliding(target) ||
+      this.boundingBox2.isColliding(target)
+    );
+  }
+
   update() {
     this.x += -6;
+    this.boundingBox1.x = this.boundingBox2.x = this.x + 30;
   }
 
   draw() {
@@ -67,5 +89,8 @@ export default class Wall {
       this.width,
       this.height
     );
+
+    this.boundingBox1.draw();
+    this.boundingBox2.draw();
   }
 }
